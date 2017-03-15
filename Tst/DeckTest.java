30011fd13f7e010c17aa60f634d5e2d6d9d5f9a2
Jory <ralexander2@ggc.edu>
2017-03-15 15:59:57 -0400

    Set up A test repository and generated Test classes for the already existing classes

---
+++ Tst/DeckTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Jory on 3/15/17.
+ */
+public class DeckTest {
+
+}
--- src/Person.java
+++ src/Person.java
@@ -2,4 +2,5 @@
  * Created by ILokhart on 3/15/2017.
  */
 public class Person {
+
 }
