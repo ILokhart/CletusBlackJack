e0641148fef962faea1bcd24958849374ef18002
Jory <ralexander2@ggc.edu>
2017-03-20 16:21:14 -0400

    Set up hand and hand test classes. The hand contains an ArrayList of card objects. You can add cards to the hand. See if a card is in a hand. Determine if the hand is empty, clear the hand. Get the toal number of points from the hand, and determine if the hand is a bust. To determine if the hand is a bust it is checked if the hand contains an ace with a value set to 11 and then changes it to 1 before calling the is bust method again. This is acommplished via the changeAce mthod and the containsAce method.

--- Tst/HandTest.java
+++ Tst/HandTest.java
@@ -8,8 +8,79 @@ import static org.junit.Assert.*;
 public class HandTest {
 
     @Test
-    public void testDraw() {
+    public void addCardTest() {
+
         Hand testHand = new Hand();
+
+        System.out.println(testHand.toString());
+        Card card = new Card("Spades",1,"Ace");
+        testHand.addCard(card);
+
+        assertEquals(true, testHand.contains(new Card("Spades",1,"Ace")));
+
+    }
+
+    @Test
+    public void clearHand() {
+        Hand testHand = new Hand();
+
+        System.out.println(testHand.toString());
+
+        Card card = new Card("Spades",1,"Ace");
+        Card card2 = new Card("Spades",1,"Ace");
+
+        testHand.addCard(card);
+        testHand.addCard(card2);
+
+        assertFalse(testHand.empty());
+        testHand.clear();
+        assertTrue(testHand.empty());
+
+
+    }
+
+    @Test
+    public void totalHandTest() {
+        Hand testHand = new Hand();
+
+        Card card1 = new Card("Spades",11,"Ace");
+        Card card2 = new Card("Hearts",5,"5");
+        Card card3 = new Card("Diamond",5,"5");
+        testHand.addCard(card1);
+        testHand.addCard(card2);
+        testHand.addCard(card3);
+
+        System.out.println(testHand.toString());
+
+        assertEquals(21,testHand.total());
+    }
+
+    @Test
+    public void isBustTest() {
+
+
+        Hand testHand = new Hand();
+
+        Card card1 = new Card("Spades",11,"Ace");
+        Card card2 = new Card("Hearts",5,"5");
+        Card card3 = new Card("Diamond",5,"5");
+        Card card4 = new Card("Clubs",10,"Jack");
+        Card card5 = new Card("Clubs",5,"5");
+        testHand.addCard(card1);
+        testHand.addCard(card2);
+        testHand.addCard(card3);
+
+        System.out.println(testHand.toString());
+
+        assertFalse(testHand.isBust());
+
+        testHand.addCard(card4);
+        assertFalse(testHand.isBust());
+
+
+        testHand.addCard(card5);
+        assertTrue(testHand.isBust());
+
 
     }
 }
--- src/Hand.java
+++ src/Hand.java
@@ -1,5 +1,91 @@
+import java.util.ArrayList;
+
 /**
  * Created by ILokhart on 3/15/2017.
  */
 public class Hand {
+
+    ArrayList<Card> cards = new ArrayList<Card>();
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    public boolean contains(Card card) {
+        boolean inHand = false;
+        for(Card x: cards)
+        {
+            if(card.getSuit().equals(x.getSuit()) && card.getSValue().equals(x.getSValue()) && card.getValue() == x.getValue()) {
+                inHand = true;
+            }
+        }
+        return inHand;
+    }
+
+    @Override
+    public String toString()
+    {
+        return cards.toString();
+    }
+
+    public boolean empty() {
+        return cards.isEmpty();
+    }
+
+    public void clear() {
+        cards.clear();
+    }
+
+    public int total() {
+        int total = 0;
+
+        for(Card x: cards) {
+
+            total = total + x.getValue();
+        }
+
+        return total;
+    }
+
+    public boolean isBust() {
+        boolean bust = false;
+        boolean containsAce = containsAce();
+        if(total() > 21 && !containsAce() ) {
+            bust =true;
+
+        }
+        else if (total() > 21 && containsAce()) {
+            changeAce();
+            bust = isBust();
+
+        }
+
+        return bust;
+    }
+
+    private void changeAce() {
+
+        for(Card x: cards)
+        {
+            if(x.getSValue().equals("Ace")) {
+                x.setValue(1);
+                return;
+            }
+        }
+
+    }
+
+    private boolean containsAce() {
+        boolean contains = false;
+
+        for(Card x: cards)
+        {
+            if(x.getSValue().equals("Ace") && x.getValue() == 11) {
+                contains = true;
+
+            }
+        }
+
+        return contains;
+    }
 }
